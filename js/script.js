d347827c5566780f79d17d74ea50473fe5a696b8
shah israil <shahisrail134@gmail.com>
2023-08-19 10:36:01 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function calculateExpense() {
+    const income = parseFloat(document.getElementById('income').value) || 0;
+    const foodExpense = parseFloat(document.getElementById('food').value) || 0;
+    const rentExpense = parseFloat(document.getElementById('rent').value) || 0;
+    const clothesExpense = parseFloat(document.getElementById('clothes').value) || 0;
+
+    const totalExpense = foodExpense + rentExpense + clothesExpense;
+    const balance = income - totalExpense;
+
+    document.getElementById('total-expense').textContent = totalExpense.toFixed(2);
+    document.getElementById('balance').textContent = balance.toFixed(2);
+}
+
+function calculateSavings() {
+    const savePercentage = parseFloat(document.getElementById('save').value) || 0;
+    const income = parseFloat(document.getElementById('income').value) || 0;
+
+    if (savePercentage < 0) {
+        alert('Savings percentage cannot be negative.');
+        return;
+    }
+    const savingAmount = (income * savePercentage) / 100;
+    const remainingBalance = income - savingAmount;
+    
+    document.getElementById('saving-amount').textContent = savingAmount.toFixed(2);
+    document.getElementById('remaining-balance').textContent = remainingBalance.toFixed(2);
+}
+
+document.getElementById('calculate-btn').addEventListener('click', calculateExpense);
+document.getElementById('save-btn').addEventListener('click', calculateSavings);
